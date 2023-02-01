20c6dc54373c61ce18d8052a24fa12e3777ffab6
Milda Zdanyte <mzzdanyt@amazon.com>
2023-02-01 14:39:19 +0000

    Add Jar file built with Maven

---
+++ src/main/java/com/napier/sem/App.java
@@ -0,0 +1,15 @@
+package com.napier.sem;
+
+public class App {
+
+    public static void main(String[] args) {
+        System.out.println("Select a query option: ");
+        System.out.println("1: All the countries in the world ....");
+        System.out.println("2: The top N populated countries ....");
+        System.out.println("3: All the cities ....");
+        System.out.println("4: The top N populated cities ....");
+        System.out.println("5: All the capital cities ....");
+        System.out.println("6: The top N populated capital cities in....");
+        System.out.println("6: The population of people, people living in cities, and people not living in cities in each....");
+    }
+}
