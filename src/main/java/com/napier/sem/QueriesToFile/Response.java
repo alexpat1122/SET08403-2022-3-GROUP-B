65ad4280bfb372344183a0f945281447f2b513cc
Milda Zdanyte <mzzdanyt@amazon.com>
2023-04-24 18:38:29 +0100

    Testing if integration and build works now

---
+++ src/main/java/com/napier/sem/QueriesToFile/Response.java
@@ -0,0 +1,28 @@
+package com.napier.sem.QueriesToFile;
+
+import com.napier.sem.structs.CapitalCity;
+import com.napier.sem.structs.City;
+import com.napier.sem.structs.Continent;
+import com.napier.sem.structs.Country;
+
+import java.sql.ResultSet;
+import java.sql.Statement;
+import java.util.ArrayList;
+
+public interface Response {
+
+    public ArrayList<String> countriesByPopDesc(String query);
+
+    public ArrayList<String> citiesByPopDesc(String query);
+
+    public ArrayList<String> capitalCitiesByPopDesc(String query);
+
+    public long pop(String col, String query);
+
+    public  int pop(String query);
+
+    public ArrayList<String> popByCity(String query);
+
+    public long populationForLanguage(String query);
+
+}
