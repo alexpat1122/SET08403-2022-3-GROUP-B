25621516e9224e55b66fa0526f89b80862f9abf0
Milda Zdanyte <mzzdanyt@amazon.com>
2023-04-25 13:47:17 +0100

    Extra integration tests

--- src/test/java/testIT.java
+++ src/test/java/testIT.java
@@ -49,6 +49,11 @@ public class testIT {
     }
 
     @Test
+    void pop2() {
+        assertEquals(103000,response.pop("population", "SELECT population FROM country LIMIT 1"));
+    }
+
+    @Test
     void pop1() {
         assertEquals(0, response.pop(""));
     }
@@ -59,7 +64,19 @@ public class testIT {
     }
 
     @Test
+    void popByCity2() {
+        assertEquals(1, response.popByCity("SELECT * FROM city WHERE name = 'Vilnius'").size());
+    }
+
+    @Test
     void popByLanguage() {
         assertEquals(0, response.populationForLanguage(""));
+    }
+
+
+    @Test
+    void popByLanguage2() {
+        assertEquals(347077866,response.populationForLanguage("select percentage, population FROM " +
+                "country JOIN countrylanguage ON country.code = countryCode WHERE language = "+ String.format("\"%s\" ", "English")));
     }
 }
